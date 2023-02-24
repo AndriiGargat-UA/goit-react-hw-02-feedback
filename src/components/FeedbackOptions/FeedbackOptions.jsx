3f47a1b84038c64ff490a5ef20462af917cfacda
Andrii Gargat <andreysax1312@gmail.com>
2023-02-24 16:41:53 +0200

    functional && prop-types add

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <ul>
+      {options.map(option => {
+        return (
+          <li key={option}>
+            <button type="button" name={option} onClick={onLeaveFeedback}>
+              {option}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
